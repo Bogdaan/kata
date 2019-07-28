33447d66e1567a7d679d03a042102e27ea3d3675
Novikov Bogdan <bogdan.novikov@socialtechnologies.io>
2019-07-28 11:26:45 +0300

    rewrite word wrap

--- src/WordWrap.php
+++ src/WordWrap.php
@@ -3,27 +3,39 @@ declare(strict_types=1);
 
 class WordWrap
 {
-    public function wrap(int $limit, string $input): string
+    public function wrap(string $input, int $limit): string
     {
-        $words = explode(' ', $input);
-        $output = '';
+        $inputLength = strlen($input);
+        $lineStartPosition = 0;
+        $lines = [];
 
-        $output .= $this->addWord($limit, array_shift($words));
-        foreach ($words as $index => $word) {
-            if (strlen($word) + 1 + strlen($output) < $limit) {
-                $output .= ' ' . $word;
-            } else {
-                $output .= PHP_EOL . $word;
+        while ($lineStartPosition < $inputLength) {
+            $nextLineStartPosition = $lineStartPosition + $limit;
+
+            $charBeforeStart = substr($input, $nextLineStartPosition - 1, 1);
+            $charAtStart = substr($input, $nextLineStartPosition, 1);
+            $charAfterStart = substr($input, $nextLineStartPosition + 1, 1);
+
+            if ($charBeforeStart !== false
+                && $charBeforeStart !== ' '
+                && $charAtStart !== ' '
+                && $charAfterStart !== false
+                && $charAfterStart !== ' '
+            ) {
+                // found some words in current line?
+                // yes: decrement to last space
+                // no: break the line
+                $spacePosition = strrpos($input, ' ', -$nextLineStartPosition);
+                if ($spacePosition !== false && $nextLineStartPosition - $spacePosition < $limit) {
+                    $nextLineStartPosition = $spacePosition + 1;
+                }
             }
+
+            $line = substr($input, $lineStartPosition, $nextLineStartPosition - $lineStartPosition);
+            $lines[] = trim($line, ' ');
+            $lineStartPosition = $nextLineStartPosition;
         }
 
-        return $output;
-    }
-
-    private function addWord(int $limit, string $word): string
-    {
-        $chunks = str_split($word, $limit);
-
-        return implode(PHP_EOL, $chunks);
+        return implode(PHP_EOL, $lines);
     }
 }
--- test/WordWrapTest.php
+++ test/WordWrapTest.php
@@ -7,32 +7,32 @@ class WordWrapTest extends TestCase
 {
     public function testSingleWordWithinLimit()
     {
-        $this->assertWrap('test', 'test', 4);
+        $this->assertWrapped('test', 'test', 4);
     }
 
     public function testSingleWordExceedsLimitOneTime()
     {
-        $this->assertWrap("super\nman", 'superman', 5);
+        $this->assertWrapped("super\nman", 'superman', 5);
     }
 
     public function testSingleWordExceedsLimitTwoTimes()
     {
-        $this->assertWrap("sup\nerm\nan", 'superman', 3);
+        $this->assertWrapped("sup\nerm\nan", 'superman', 3);
     }
 
     public function testSecondWordWrapped()
     {
-        $this->assertWrap("to\nLondon", 'to London', 6);
+        $this->assertWrapped("to\nLondon", 'to London', 6);
     }
 
     public function testSingleLineBrakeAndSecondWordSplit()
     {
-        $this->assertWrap("to\nLond\non", 'to London', 4);
+        $this->assertWrapped("to\nLond\non", 'to London', 4);
     }
 
-    private function assertWrap(string $expected, string $input, int $limit): void
+    private function assertWrapped(string $expected, string $input, int $limit): void
     {
         $wrap = new WordWrap();
-        $this->assertSame($expected, $wrap->wrap($limit, $input));
+        $this->assertSame($expected, $wrap->wrap($input, $limit));
     }
 }
