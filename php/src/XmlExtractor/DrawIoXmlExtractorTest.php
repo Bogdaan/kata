822fb9265c7e93e898ffdcea40e26af8a59395bd
Novikov Bogdan <bogdan.novikov@socialtechnologies.io>
2019-08-09 10:45:43 +0300

    update XmlExtractorTest to concreate case

--- php/src/XmlExtractor/DrawIoXmlExtractorTest.php
+++ php/src/XmlExtractor/DrawIoXmlExtractorTest.php
@@ -9,24 +9,34 @@ use Kata\XmlExtractor\samples\EmptyClass;
 use Kata\XmlExtractor\samples\SingleMethod;
 use PHPUnit\Framework\TestCase;
 
-class XmlExtractorTest extends TestCase
+class DrawIoXmlExtractorTest extends TestCase
 {
+    /**
+     * @var XmlExtractor
+     */
+    private $drawIoExtractor;
+
+    protected function setUp()
+    {
+        $this->drawIoExtractor = new XmlExtractor(new ReflectionGroupExtractor(), new DrawIoXmlFormatter());
+    }
+
     public function testExtractFromClassWithoutMethods(): void
     {
         $sourceClass = EmptyClass::class;
-        $xml = <<<XML
+        $expected = <<<XML
         <mxCell id="root-id" value="{$sourceClass}" style="swimlane;" parent="1" vertex="1">
           <mxGeometry x="0" y="0" width="200" height="200" as="geometry"/>
         </mxCell>
 XML;
 
-        $this->assertExtracted($xml, $sourceClass);
+        $this->assertExtracted($expected, $sourceClass);
     }
 
     public function testExtractFromClassWithSingleMethod(): void
     {
         $sourceClass = SingleMethod::class;
-        $xml = <<<XML
+        $expected = <<<XML
         <mxCell id="root-id" value="{$sourceClass}" style="swimlane;" parent="1" vertex="1">
           <mxGeometry x="0" y="0" width="200" height="200" as="geometry"/>
         </mxCell>
@@ -35,16 +45,14 @@ XML;
         </mxCell>
 XML;
 
-        $this->assertExtracted($xml, $sourceClass);
+        $this->assertExtracted($expected, $sourceClass);
     }
 
     private function assertExtracted(string $expectedXml, string $sourceClass): void
     {
-        $extractor = new XmlExtractor(new ReflectionGroupExtractor(), new DrawIoXmlFormatter());
-
         $this->assertContains(
             $this->normalize($expectedXml),
-            $this->normalize($extractor->extract($sourceClass))
+            $this->normalize($this->drawIoExtractor->extract($sourceClass))
         );
     }
 
