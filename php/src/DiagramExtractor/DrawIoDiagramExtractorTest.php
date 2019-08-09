217f2d2df22e1e1cc1f196c240a4237bc07b070f
Novikov Bogdan <bogdan.novikov@socialtechnologies.io>
2019-08-09 11:29:35 +0300

    move substring xml test to xpath matching

--- php/src/DiagramExtractor/DrawIoDiagramExtractorTest.php
+++ php/src/DiagramExtractor/DrawIoDiagramExtractorTest.php
@@ -25,62 +25,48 @@ class DrawIoDiagramExtractorTest extends TestCase
     public function testExtractFromClassWithoutMethods(): void
     {
         $sourceClass = EmptyClass::class;
-        $expected = <<<XML
-        <mxCell id="root-id" value="{$sourceClass}" style="swimlane;" parent="1" vertex="1">
-          <mxGeometry x="0" y="0" width="200" height="200" as="geometry"/>
-        </mxCell>
-XML;
+        $xpath = [
+            sprintf('//root/mxCell[@value="%s"][@id="root-id"]/mxGeometry', $sourceClass),
+        ];
 
-        $this->assertExtracted($expected, $sourceClass);
+        $this->assertExtractedAs($xpath, $sourceClass);
     }
 
     public function testExtractFromClassWithSingleMethod(): void
     {
         $sourceClass = SingleMethod::class;
-        $expected = <<<XML
-        <mxCell id="root-id" value="{$sourceClass}" style="swimlane;" parent="1" vertex="1">
-          <mxGeometry x="0" y="0" width="200" height="200" as="geometry"/>
-        </mxCell>
-        <mxCell value="Get string with current unix time" style="ellipse;whiteSpace=wrap;html=1;" parent="root-id" vertex="1">
-          <mxGeometry x="1" y="1" width="200" height="70" as="geometry"/>
-        </mxCell>
-XML;
+        $xpath = [
+            sprintf(
+                '//root/mxCell[@value="%s"][@parent="root-id"]/mxGeometry',
+                'Get string with current unix time'
+            ),
+        ];
 
-        $this->assertExtracted($expected, $sourceClass);
+        $this->assertExtractedAs($xpath, $sourceClass);
     }
 
     public function testExtractFromClassWithMultipleMethodsWithInheritance(): void
     {
         $sourceClass = MultipleMethods::class;
-        $expected = <<<XML
-        <mxCell id="root-id" value="{$sourceClass}" style="swimlane;" parent="1" vertex="1">
-          <mxGeometry x="0" y="0" width="200" height="200" as="geometry"/>
-        </mxCell>
-        <mxCell value="Get random number" style="ellipse;whiteSpace=wrap;html=1;" parent="root-id" vertex="1">
-          <mxGeometry x="1" y="1" width="200" height="70" as="geometry"/>
-        </mxCell>
-        <mxCell value="Get string with current unix time" style="ellipse;whiteSpace=wrap;html=1;" parent="root-id" vertex="1">
-          <mxGeometry x="1" y="1" width="200" height="70" as="geometry"/>
-        </mxCell>
-XML;
+        $xpath = [
+            sprintf(
+                '//root/mxCell[@value="%s"][@parent="root-id"]/mxGeometry',
+                'Get string with current unix time'
+            ),
+            sprintf(
+                '//root/mxCell[@value="%s"][@parent="root-id"]/mxGeometry',
+                'Get random number'
+            ),
+        ];
 
-        $this->assertExtracted($expected, $sourceClass);
+        $this->assertExtractedAs($xpath, $sourceClass);
     }
 
-    private function assertExtracted(string $expectedXml, string $sourceClass): void
+    private function assertExtractedAs(array $xpathList, string $sourceClass): void
     {
-        $this->assertContains(
-            $this->normalize($expectedXml),
-            $this->normalize($this->drawIoExtractor->extract($sourceClass))
-        );
-    }
-
-    private function normalize(string $multilineText): string
-    {
-        return preg_replace(
-            "/\s+/",
-            "",
-            preg_replace("/\n/", "", $multilineText)
-        );
+        $diagramXml = simplexml_load_string($this->drawIoExtractor->extract($sourceClass));
+        foreach ($xpathList as $xpath) {
+            $this->assertNotEmpty($diagramXml->xpath($xpath));
+        }
     }
 }
